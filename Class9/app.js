2392584bfb43f4bae2a13d03042311f0ce8f05b5
Saira Batool <sairabatool787@gmail.com>
2024-06-04 08:43:19 +0500

    Done

---
+++ Class9/app.js
@@ -0,0 +1,28 @@
+//Palindromes Words
+var word = prompt("Enter Any Word");
+var reverse = "";
+for (var i = 0; i < word.length; i++) {
+    console.log(word[i]) // saira in every index from 0 to 4 
+    // console.log(i)  // 0-4 index
+}
+
+for (var i = word.length - 1; i >= 0; i--) {
+    console.log(word[i]) //ariar in every index
+}
+
+for (var i = word.length - 1; i >= 0; i--) {
+    reverse += word[i] 
+  //var declared upon
+//   console.log(reverse) 
+//   i
+//   il 
+//   ila
+}
+console.log(reverse) //anas 
+
+for (var i = 1; i <= 10; i++) {
+    console.log(i)
+}
+for (var i = 10; i >= 1; i--) {
+    console.log(i)
+}
