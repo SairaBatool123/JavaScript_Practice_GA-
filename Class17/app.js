4f357a7bbeaa0a1e5a6bed1b69273e46bd2f7fde
Saira Batool <sairabatool787@gmail.com>
2024-07-12 14:29:16 +0500

    Done

---
+++ Class17/app.js
@@ -0,0 +1,29 @@
+// // switch is used for matching a single value
+
+// var day = prompt("Enter Your Day")
+// day = day.toLowerCase
+// switch (day) {
+//     case "tues":
+//         console.log("Theory");
+//         break
+//     case "thur":
+//         console.log("Practical")
+//         break
+//     case "Fri":
+//         console.log("Paper");
+//         break
+//     default:
+//         console.log("off")
+
+// }
+function abc(){
+    var myStr = "Hello World" //local
+}
+var myStr = "Hello World" //global
+
+console.log(document) //global
+console.log(document.getElementById("main"));
+console.log(window) //global
+
+
+// while loop
